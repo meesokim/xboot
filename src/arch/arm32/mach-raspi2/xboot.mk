aaa26b0befaaf7d47047b03d127cc70a1d288bb7
Miso Kim <meeso.kim@gmail.com>
2020-03-16 23:07:18 +0900

    update for raspi2

--- src/arch/arm32/mach-raspi2/xboot.mk
+++ src/arch/arm32/mach-raspi2/xboot.mk
@@ -6,11 +6,11 @@ DEFINES		+= -D__ARM32_ARCH__=7 -D__CORTEX_A7__ -D__ARM32_NEON__
 
 ASFLAGS		:= -g -ggdb -Wall -O3
 CFLAGS		:= -g -ggdb -Wall -O3
-LDFLAGS		:= -T arch/$(ARCH)/$(MACH)/xboot.ld -nostdlib
 MCFLAGS		:= -march=armv7-a -mtune=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard -marm -mno-thumb-interwork
+LDFLAGS		:= -T arch/$(ARCH)/$(MACH)/xboot.ld -nostdlib
 
 LIBDIRS		:=
-LIBS 		:=
+LIBS 		:= `$(CROSS_COMPILE)gcc -print-file-name=libgcc.a $(MCFLAGS)`
 INCDIRS		:=
 SRCDIRS		:=
 
